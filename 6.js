0caee5b9add5f2dd36f18609ba4643f1ba20218a
somya <somyasharma0488@gmail.com>
2025-11-15 09:49:39 +0530

    main

---
+++ 6.js
@@ -0,0 +1,21 @@
+function a(...abc) {
+    console.log(abc)
+}
+
+a(2, 3, 5);
+
+
+//Temperal Dead Zone Example
+
+//reverse a string
+const str = "Hello, World!";
+const reversedStr = str.split('').reverse().join('');
+console.log(reversedStr); // Output: !dlroW ,olleH
+
+
+//Condition and corcion
+if ([]==emptyArray) {
+    console.log("This is true");
+} else {
+    console.log("This is false");
+}
